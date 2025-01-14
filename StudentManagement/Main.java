daef5fd405076021ef9add922ba490fef2276608
Adarsh <np03cs4s240013@heraldcollege.edu.np>
2025-01-14 13:27:24 +0545

    Adding getter and setter method to Student class

---
+++ StudentManagement/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public Main() {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+
+    public static void main(String args[]){
+        System.out.println("Hey , I'm a Java program!");
+    }
+}
---
+++ StudentManagement/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    private String name;
+    private int age;
+    private String grade;
+    private String subject;
+
+    public Student(String name, int age){
+        this.name=name;
+        this.age=age;
+    }
+    public static void main(String[] args) {
+        String name;
+        int age;
+
+    }
+}
